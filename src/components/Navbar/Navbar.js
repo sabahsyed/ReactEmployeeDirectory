1db24e5c8c68a463a4cd3a82f289158a054f313f
Sabah Syed <sabisyed@gmail.com>
2020-07-22 05:58:42 -0700

    test

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import "./Navbar.css"
     function Navbar() {
         return (
             <div>
-                <nav class ="navbar navbar-dark bg-dark">
+                <nav className ="navbar navbar-dark bg-dark">
                 <h1 className = "h1" >Employee Directory</h1>
                 </nav>
             </div>
